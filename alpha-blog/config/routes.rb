d98a7067e62c36fb0ce531da4a3caa976690da96
Richard Hoyos <rchrdhysdev@gmail.com>
2018-02-13 05:54:36 -0500

    Start alpha-blog

---
+++ alpha-blog/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #Method(GET,POST,UPDATE;DELETE) PATH to CONTROLLER#ACTION 
+  root 'pages#home'
+
+  get 'about', to: 'pages#about'
+end
