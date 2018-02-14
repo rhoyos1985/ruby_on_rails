79eca27f4eec7a7b02d8d2be04a8759b8a4c4cde
Richard Hoyos <rchrdhysdev@gmail.com>
2018-02-14 09:15:03 -0500

    Update articles and routes articles

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -14,11 +14,32 @@ class ArticlesController < ApplicationController
           render 'new'
         end
     end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def index
+        @articles = Article.all
+    end
+    
 
     def show
         @article = Article.find(params[:id])
     end
-     
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update_attributes(article_params)
+          flash[:success] = "Article was successfully updated"
+          redirect_to @article
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+    
+
     private
         def article_params
             params.require(:article).permit(:title, :description)
