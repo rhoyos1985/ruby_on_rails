2d97149a7ccbd9bf6d0e9010ed1826294fd10c30
Richard Hoyos <rchrdhysdev@gmail.com>
2018-02-16 09:47:47 -0500

    Create destroy method

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -15,6 +15,13 @@ class ArticlesController < ApplicationController
         end
     end
     
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy ? flash[:success] = 'Object was successfully deleted.' : flash[:error] = 'Something went wrong'
+        redirect_to articles_path
+    end
+    
+
     def edit
         @article = Article.find(params[:id])
     end
