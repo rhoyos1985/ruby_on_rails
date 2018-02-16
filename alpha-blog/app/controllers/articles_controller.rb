8f19169facfff6103cafd91968b5323560a4a510
Richard Hoyos <rchrdhysdev@gmail.com>
2018-02-16 10:06:26 -0500

    Extract reduncy and prepare to production

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -1,56 +1,58 @@
 class ArticlesController < ApplicationController
-    
-    def new
-        @article = Article.new
-    end
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-    def create
-        @article = Article.new(article_params)
-        if @article.save
-          flash[:success] = "Object successfully created"
-          redirect_to @article
-        else
-          flash[:error] = "Something went wrong"
-          render 'new'
-        end
-    end
-    
-    def destroy
-        @article = Article.find(params[:id])
-        @article.destroy ? flash[:success] = 'Object was successfully deleted.' : flash[:error] = 'Something went wrong'
-        redirect_to articles_path
-    end
-    
+	def new
+			@article = Article.new
+	end
 
-    def edit
-        @article = Article.find(params[:id])
-    end
-    
-    def index
-        @articles = Article.all
-    end
-    
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:success] = "Object successfully created"
+			redirect_to @article
+		else
+			flash[:error] = "Something went wrong"
+			render 'new'
+		end
+	end
+	
+	def destroy
+		@article.destroy ? flash[:success] = 'Object was successfully deleted.' : flash[:error] = 'Something went wrong'
+		redirect_to articles_path
+	end
+	
 
-    def show
-        @article = Article.find(params[:id])
-    end
+	def edit
+			
+	end
+	
+	def index
+		@articles = Article.all
+	end
+	
 
-    def update
-        @article = Article.find(params[:id])
-        if @article.update_attributes(article_params)
-          flash[:success] = "Article was successfully updated"
-          redirect_to @article
-        else
-          flash[:error] = "Something went wrong"
-          render 'edit'
-        end
-    end
-    
+	def show
+			
+	end
 
-    private
-        def article_params
-            params.require(:article).permit(:title, :description)
-        end
-    
+	def update
+		if @article.update_attributes(article_params)
+			flash[:success] = "Article was successfully updated"
+			redirect_to @article
+		else
+			flash[:error] = "Something went wrong"
+			render 'edit'
+		end
+	end
+	
+
+	private
+		def set_article
+			@article = Article.find(params[:id])
+		end
+
+		def article_params
+			params.require(:article).permit(:title, :description)
+		end
 
 end
