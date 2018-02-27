8b11f0807c0a7fa86acfadbbc3f8a54080be1ba6
Richard Hoyos <richard@dentalzon.com>
2018-02-27 04:00:45 -0500

    Edit view and controller

--- alpha-blog/app/controllers/users_controller.rb
+++ alpha-blog/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :edit, :update, :destroy ] 
   
   def new
     @user = User.new
@@ -15,7 +16,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      flash[:error] = "Your account was not updated"
+      render 'edit'
+    end
+  end
+
+  def destroy
+  
+  end
+
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.required(:user).permit(:username, :email, :password)
     end
