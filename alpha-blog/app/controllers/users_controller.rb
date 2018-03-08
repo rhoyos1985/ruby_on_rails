f503c1a24ad898775fae5c4197de3b29e4e85b33
Richard Hoyos <richard@dentalzon.com>
2018-03-08 17:31:16 -0500

    Finished the first app with ruby on rails

--- alpha-blog/app/controllers/users_controller.rb
+++ alpha-blog/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
-  before_action :require_same_user, only: %i[edit update]
+  before_action :require_same_user, only: %i[edit update destroy]
+  before_action :require_admin, only: [:destroy]
 
   def new
     @user = User.new
@@ -9,15 +10,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       flash[:error] = "The user hasn't be created"
       render 'new'
     end
   end
 
-  def destroy; end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = "User and all articles created by user have been deleted"
+    redirect_to users_path
+  end
 
   def edit; end
 
@@ -50,9 +57,16 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:error] = 'You can only edit your own account'
       redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:error] = "Only admin users can perform that action"
+      redirec_to root_path
     end
   end
 end
