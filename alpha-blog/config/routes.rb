9bd0243f45499f2b31b46426be02ab519444e3be
Richard Hoyos <richard@dentalzon.com>
2018-03-09 16:01:49 -0500

    Added Unit test for the model and controller of categories

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #Method(GET,POST,UPDATE;DELETE) PATH to CONTROLLER#ACTION 
+  # Method(GET,POST,UPDATE;DELETE) PATH to CONTROLLER#ACTION 
   root 'pages#home'
   get 'about', to: 'pages#about'
 
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
 end
