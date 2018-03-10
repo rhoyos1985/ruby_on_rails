75e4156a4d5d8f33965e3429cd168c4d004b2fee
Richard Hoyos <richard@dentalzon.com>
2018-03-10 06:57:06 -0500

    The unit test is added for Integration Test

--- alpha-blog/app/controllers/categories_controller.rb
+++ alpha-blog/app/controllers/categories_controller.rb
@@ -1,8 +1,34 @@
 class CategoriesController < ApplicationController
-  def new
+  before_action :set_category, only: [:show]
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = 'Category successfully created'
+      redirect_to categories_path
+    else
+      flash[:error] = 'Something went wrong'
+      render 'new'
+    end
   end
+
   def index
+    @categories = Category.all
   end
-  def show
+
+  def new
+    @category = Category.new
+  end
+
+  def show; end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
