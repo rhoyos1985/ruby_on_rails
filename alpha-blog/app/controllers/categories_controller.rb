4457e5aea7dae5f32805acd900de5fe7edf6cb71
Richard Hoyos <richard@dentalzon.com>
2018-03-11 09:05:55 -0500

    Update navbar to add categories

--- alpha-blog/app/controllers/categories_controller.rb
+++ alpha-blog/app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show]
+  before_action :require_admin, except: %i[index show]
 
   def create
     @category = Category.new(category_params)
@@ -13,7 +14,7 @@ class CategoriesController < ApplicationController
   end
 
   def index
-    @categories = Category.all
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -30,5 +31,12 @@ class CategoriesController < ApplicationController
 
   def set_category
     @category = Category.find(params[:id])
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? && !current_user.admin?)
+      flash[:error] = 'Only admins can perform that action'
+      redirect_to categories_path
+    end
   end
 end
